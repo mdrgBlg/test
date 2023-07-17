5c894d57452b26cc36e52c764c7a8e1c026cad39
mdrgBlg <mdrgblg@gmail.com>
2023-07-17 12:43:42 +0200

    users_first_version

---
+++ src/usersScreen/Users.tsx
@@ -0,0 +1,34 @@
+import  {useState,useEffect} from "react";
+import axios from "axios";
+
+
+
+const Users = () => {
+
+interface Data {
+    name: string;
+    username: string;
+    email: string;
+    phone:string;
+}
+
+const [data, setData] = useState<Data[]>([]);
+const url = "https://jsonplaceholder.typicode.com/users";
+
+
+const fetchInfo = () => {
+
+    return axios.get(url).then((response) => setData(response.data)).catch(err =>{console.log(err)});
+
+}
+
+useEffect(() => {
+    fetchInfo();
+}, [])
+
+
+return data;
+
+}
+
+export default Users;
